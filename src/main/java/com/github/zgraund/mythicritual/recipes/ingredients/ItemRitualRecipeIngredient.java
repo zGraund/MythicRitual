55f5f238069392eb5b3cc52557e0433c530629ff
zGraund <leo.gaud@hotmail.it>
2025-08-27 23:48:41 +0200

    added quantity range

--- src/main/java/com/github/zgraund/mythicritual/recipes/ingredients/ItemRitualRecipeIngredient.java
+++ src/main/java/com/github/zgraund/mythicritual/recipes/ingredients/ItemRitualRecipeIngredient.java
@@ -1,12 +1,12 @@
 package com.github.zgraund.mythicritual.recipes.ingredients;
 
 import com.github.zgraund.mythicritual.util.EntityConsumer;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
@@ -21,7 +21,7 @@ public record ItemRitualRecipeIngredient(
 ) implements RitualRecipeIngredient {
     public static final MapCodec<ItemRitualRecipeIngredient> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
                     ResourceLocation.CODEC.fieldOf("type").forGetter(ItemRitualRecipeIngredient::type),
-                    Codec.INT.optionalFieldOf("quantity", 1).forGetter(ItemRitualRecipeIngredient::quantity),
+                    ExtraCodecs.intRange(1, 99).fieldOf("quantity").orElse(1).forGetter(ItemRitualRecipeIngredient::quantity),
                     Vec3i.CODEC.optionalFieldOf("pos", new Vec3i(0, 1, 0)).forGetter(ItemRitualRecipeIngredient::offset)
             ).apply(inst, ItemRitualRecipeIngredient::new)
     );
