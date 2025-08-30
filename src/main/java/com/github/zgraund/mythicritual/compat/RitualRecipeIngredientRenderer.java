893681b7d07c78e6ef78005e3748aa12d9207de0
zGraund <leo.gaud@hotmail.it>
2025-08-30 05:04:17 +0200

    initial jei render (WIP)

--- src/main/java/com/github/zgraund/mythicritual/compat/RitualRecipeIngredientRenderer.java
+++ src/main/java/com/github/zgraund/mythicritual/compat/RitualRecipeIngredientRenderer.java
@@ -4,16 +4,12 @@ import com.github.zgraund.mythicritual.recipes.ingredients.ItemRitualRecipeIngre
 import com.github.zgraund.mythicritual.recipes.ingredients.MobRitualRecipeIngredient;
 import com.github.zgraund.mythicritual.recipes.ingredients.RitualRecipeIngredient;
 import mezz.jei.api.ingredients.IIngredientRenderer;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
 import java.util.List;
 
@@ -23,19 +19,20 @@ public record RitualRecipeIngredientRenderer() implements IIngredientRenderer<Ri
         switch (ingredient) {
             case ItemRitualRecipeIngredient item -> guiGraphics.renderItem(item.asItemStack(), 0, 0);
             case MobRitualRecipeIngredient entity -> {
-                if (Minecraft.getInstance().level != null) {
-                    LivingEntity toRender = (LivingEntity) entity.asEntityType().create(Minecraft.getInstance().level);
-                    if (toRender instanceof Mob m) m.setNoAi(true);
-                    InventoryScreen.renderEntityInInventory(
-                            guiGraphics,
-                            8, 15, 18,
-//                            new Vector3f(0.45f, 0.8f, 0),
-                            new Vector3f(0.0f, 0.0f, 0.0f),
-                            new Quaternionf().rotationXYZ((float) Math.PI, 0, 0.0F),
-                            null,
-                            toRender
-                    );
-                }
+                guiGraphics.renderItem(new ItemStack(Items.DEBUG_STICK), 0, 0);
+//                if (Minecraft.getInstance().level != null) {
+//                    LivingEntity toRender = (LivingEntity) entity.asEntityType().create(Minecraft.getInstance().level);
+//                    if (toRender instanceof Mob m) m.setNoAi(true);
+//                    InventoryScreen.renderEntityInInventory(
+//                            guiGraphics,
+//                            8, 15, 18,
+////                            new Vector3f(0.45f, 0.8f, 0),
+//                            new Vector3f(0.0f, 0.0f, 0.0f),
+//                            new Quaternionf().rotationXYZ((float) Math.PI, 0, 0.0F),
+//                            null,
+//                            toRender
+//                    );
+//                }
             }
         }
     }
--- src/main/java/com/github/zgraund/mythicritual/compat/RitualRecipeJEICategory.java
+++ src/main/java/com/github/zgraund/mythicritual/compat/RitualRecipeJEICategory.java
@@ -2,11 +2,13 @@ package com.github.zgraund.mythicritual.compat;
 
 import com.github.zgraund.mythicritual.MythicRitual;
 import com.github.zgraund.mythicritual.recipes.RitualRecipe;
-import com.github.zgraund.mythicritual.recipes.ingredients.MobRitualRecipeIngredient;
+import com.github.zgraund.mythicritual.recipes.ingredients.RitualRecipeIngredient;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.placement.VerticalAlignment;
 import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
@@ -14,7 +16,6 @@ import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -23,67 +24,66 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 public class RitualRecipeJEICategory implements IRecipeCategory<RitualRecipe> {
     public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(MythicRitual.MOD_ID, "ritual_recipe");
-    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(MythicRitual.MOD_ID, "textures/gui/ritual_recipe/image.png");
+    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(MythicRitual.MOD_ID, "textures/gui/gui_test_2.png");
     public static final RecipeType<RitualRecipe> RITUAL_RECIPE_JEI_TYPE = new RecipeType<>(UID, RitualRecipe.class);
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final int width = 162;
+    private final int height = 144;
 
     public RitualRecipeJEICategory(@NotNull IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE, 0, 0, 175, 165);
+        this.background = helper.createDrawable(TEXTURE, 0, 0, width, height);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Items.ANVIL));
-    }
-
-    @SuppressWarnings("removal")
-    @Override
-    public @Nullable IDrawable getBackground() {
-        return background;
-    }
-
-    @Override
-    @Nonnull
-    public RecipeType<RitualRecipe> getRecipeType() {
-        return RITUAL_RECIPE_JEI_TYPE;
-    }
-
-    @Override
-    @Nonnull
-    public Component getTitle() {
-        return Component.literal("Ritual");
-    }
-
-    @Override
-    public @Nullable IDrawable getIcon() {
-        return icon;
     }
 
     @Override
     public void createRecipeExtras(IRecipeExtrasBuilder builder, RitualRecipe recipe, IFocusGroup focuses) {
-        IRecipeCategory.super.createRecipeExtras(builder, recipe, focuses);
+        Component text = Component.literal("Ritual Ingredients: ");
+        builder.addText(text, 108, 20)
+               .setPosition(19, 54)
+               .setTextAlignment(VerticalAlignment.CENTER);
+
+        List<IRecipeSlotDrawable> inputSlots = builder.getRecipeSlots().getSlots(RecipeIngredientRole.INPUT);
+        inputSlots.subList(0, 2).clear();
+
+        builder.addScrollGridWidget(inputSlots, 6, 3).setPosition(18, 72);
+//        builder.addScrollGridWidget(inputSlots, 7, 4).setPosition(18, 72);
     }
 
     @Override
     public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull RitualRecipe recipe, @NotNull IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 25, 50).addItemStack(recipe.trigger());
-        builder.addSlot(RecipeIngredientRole.INPUT, 50, 50).addItemLike(recipe.target().getBlock().asItem());
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 75, 50).addItemStack(recipe.result());
-        builder
-                .addInputSlot(50, 75)
-                .addIngredient(RitualRecipeJEIIngredient.RITUAL_RECIPE_INGREDIENT_JEI_TYPE, recipe
-                        .ingredients()
-                        .stream()
-                        .filter(f -> f instanceof MobRitualRecipeIngredient)
-                        .findFirst()
-                        .orElse(new MobRitualRecipeIngredient(ResourceLocation.parse("minecraft:zombie"), new Vec3i(0, 1, 0)))
-                );//.addRichTooltipCallback((recipeSlotView, tooltip) -> tooltip.add());
+        builder.addInputSlot().setPosition(19, 19).addItemStack(recipe.trigger());
+        builder.addInputSlot().setPosition(73, 19).addItemLike(recipe.target().getBlock().asItem());
+        builder.addOutputSlot().setPosition(127, 19).addItemStack(recipe.result());
+        for (RitualRecipeIngredient ingredient : recipe.ingredients()) {
+            builder.addInputSlot().addIngredient(RitualRecipeJEIIngredient.RITUAL_RECIPE_INGREDIENT_JEI_TYPE, ingredient);
+        }
     }
 
     @Override
     public void draw(@NotNull RitualRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
-//        IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
-        background.draw(guiGraphics);
+//        background.draw(guiGraphics);
     }
+
+    @Override
+    public int getHeight() {return height;}
+
+    @Override
+    public int getWidth() {return width;}
+
+    @Override
+    @Nonnull
+    public RecipeType<RitualRecipe> getRecipeType() {return RITUAL_RECIPE_JEI_TYPE;}
+
+    @Override
+    @Nonnull
+    public Component getTitle() {return Component.literal("Ritual");}
+
+    @Override
+    public @Nullable IDrawable getIcon() {return icon;}
 }
