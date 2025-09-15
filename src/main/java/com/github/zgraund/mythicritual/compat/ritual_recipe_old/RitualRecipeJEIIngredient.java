9530d04c44be008927cba95eebce09519ae04ebc
zGraund <leo.gaud@hotmail.it>
2025-09-15 03:39:55 +0200

    deleted old recipe system

--- src/main/java/com/github/zgraund/mythicritual/compat/ritual_recipe_old/RitualRecipeJEIIngredient.java
+++
@@ -1,8 +0,0 @@
-package com.github.zgraund.mythicritual.compat.ritual_recipe_old;
-
-import com.github.zgraund.mythicritual.recipes_old.ingredients.RitualRecipeOffering;
-import mezz.jei.api.ingredients.IIngredientType;
-
-public record RitualRecipeJEIIngredient() {
-    public static final IIngredientType<RitualRecipeOffering> TYPE = () -> RitualRecipeOffering.class;
-}
